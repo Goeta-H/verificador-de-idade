9434d5397807bce9aa11de94d7fce840d233e7d6
Helen Christine <68754831+goetah@users.noreply.github.com>
2021-08-23 15:23:36 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,69 +15,69 @@ function check() {
             gender = ' a Diversidade'
             if (age >= 0 && age < 10) {
                 //kid
-                img.setAttribute('src', 'images/lgbtqi.png')
+                img.setAttribute('src', './images/lgbtqi.png')
             } else if (age < 23) {
                 //Jovem
-                img.setAttribute('src', 'images/young-lgbtqi.png')
+                img.setAttribute('src', './images/young-lgbtqi.png')
             } else if (age < 50) {
                 //Adulto
-                img.setAttribute('src', 'images/adult-lgbtqi.png')
+                img.setAttribute('src', './images/adult-lgbtqi.png')
             } else if (age < 110) {
                 //Idoso
-                img.setAttribute('src', 'images/old-lgbtqi.png')
+                img.setAttribute('src', './images/old-lgbtqi.png')
             } else {
-                img.setAttribute('src', 'images/die.png')
+                img.setAttribute('src', './images/die.png')
             }
         } else if (fsex[1].checked) {
             gender = 'uma Mulher'
             if (age >= 0 && age < 10) {
                 //kid
-                img.setAttribute('src', 'images/kid-girl.png')
+                img.setAttribute('src', './images/kid-girl.png')
             } else if (age < 23) {
                 //Jovem
-                img.setAttribute('src', 'images/young-girl.png')
+                img.setAttribute('src', './images/young-girl.png')
             } else if (age < 50) {
                 //Adulto
-                img.setAttribute('src', 'images/adult-women.png')
+                img.setAttribute('src', './images/adult-women.png')
             } else if (age < 100) {
                 //idoso
-                img.setAttribute('src', 'images/old-women.png')
+                img.setAttribute('src', './images/old-women.png')
             } else {
-                img.setAttribute('src', 'images/die.png')
+                img.setAttribute('src', './images/die.png')
             }
         } else if (fsex[2].checked) {
             gender = 'um Homem'
             if (age >= 0 && age < 10) {
                 //kid
-                img.setAttribute('src', 'images/kid-boy.png')
+                img.setAttribute('src', './images/kid-boy.png')
             } else if (age < 23) {
                 //Jovem
-                img.setAttribute('src', 'images/young-boy.png')
+                img.setAttribute('src', './images/young-boy.png')
             } else if (age < 50) {
                 //Adulto
-                img.setAttribute('src', 'images/adult-men.png')
+                img.setAttribute('src', './images/adult-men.png')
             } else if (age < 110) {
                 //Velho mesmo
-                img.setAttribute('src', 'images/old-men.png')
+                img.setAttribute('src', './images/old-men.png')
             } else {
-                img.setAttribute('src', 'images/die.png')
+                img.setAttribute('src', './images/die.png')
             }
         } else if (fsex[3].checked) {            
             if (age >= 0 && age < 10) {
                 gender = 'uma Pedra'
-                img.setAttribute('src', 'images/rock.png')
+                img.setAttribute('src', './images/rock.png')
             } else if (age < 23) {
                 gender = 'um Girassol'
-                img.setAttribute('src', 'images/sun.png')
+                img.setAttribute('src', './images/sun.png')
             } else if (age < 50) {
                 gender = 'uma Árvore'
-                img.setAttribute('src', 'images/tree.png')
+                img.setAttribute('src', './images/tree.png')
             } else if (age < 110) {
                 gender = 'uma Múmia'
-                img.setAttribute('src', 'images/mummy.png')
+                img.setAttribute('src', './images/mummy.png')
             } else {
                 gender = 'um Dinossauro'
-                img.setAttribute('src', 'images/dino.png')
+                img.setAttribute('src', './images/dino.png')
             }            
         }
         res.style.textAlign = 'center'
